6f65fe0c917446c66946758a73225cc8fc028ea9
Jorgecdl96 <100006802+Jorgecdl96@users.noreply.github.com>
2022-08-04 18:55:28 -0600

    Footer section

---
+++ portfolio/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { IoLogoLinkedin, IoLogoGithub, IoMail, IoLogoStackoverflow  } from "react-icons/io5";
+import '../styles/Footer.css';
+
+function Footer() {
+  return (
+    <div className='footer'>
+        <div className='footerIcons'>
+            <a href='https://www.linkedin.com/in/jorgecdl96/' target='_blank' rel='noopener noreferrer'><IoLogoLinkedin /></a>
+            <a href='https://github.com/Jorgecdl96' target='_blank' rel='noopener noreferrer'><IoLogoGithub/></a>
+            <a href='https://stackoverflow.com/users/18249123/jorgehcl96?tab=profile' target='_blank' rel='noopener noreferrer'><IoLogoStackoverflow/></a>
+            <IoMail/>
+        </div>
+        <p> &copy; 2022 Jorge Cardenas </p>
+    </div>
+  )
+}
+
+export default Footer
