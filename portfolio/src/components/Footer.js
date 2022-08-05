3353f60fa2f6e6e335e506a7a210c02ef2079f1d
Jorgecdl96 <100006802+Jorgecdl96@users.noreply.github.com>
2022-08-04 19:26:58 -0600

    adding opacity to footer

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IoLogoLinkedin, IoLogoGithub, IoMail, IoLogoStackoverflow  } from "react-icons/io5";
+import { IoLogoLinkedin, IoLogoGithub, IoLogoStackoverflow  } from "react-icons/io5";
 import '../styles/Footer.css';
 
 function Footer() {
@@ -9,7 +9,6 @@ function Footer() {
             <a href='https://www.linkedin.com/in/jorgecdl96/' target='_blank' rel='noopener noreferrer'><IoLogoLinkedin /></a>
             <a href='https://github.com/Jorgecdl96' target='_blank' rel='noopener noreferrer'><IoLogoGithub/></a>
             <a href='https://stackoverflow.com/users/18249123/jorgehcl96?tab=profile' target='_blank' rel='noopener noreferrer'><IoLogoStackoverflow/></a>
-            <IoMail/>
         </div>
         <p> &copy; 2022 Jorge Cardenas </p>
     </div>
